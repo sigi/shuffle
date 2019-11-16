749f5adee6abeb71ee02e34becb7fb47f66c6064
Frank 'Sigi' Luithle <frank@luithle.net>
2019-11-16 18:18:45 +0100

    Display 52 arrows in a web browser
    Amazing.

---
+++ main.js
@@ -0,0 +1,38 @@
+var grid, randomDeal, shuffle
+
+const { el, list, mount, setAttr } = redom
+
+shuffle = function(a) {
+    var i, j, len
+    len = a.length
+    if (len > 1) {
+        for (i = len - 1; i >= 1; --i) {
+            j = Math.floor(Math.random() * (i + 1))
+            ;[ a[i], a[j] ] = [ a[j], a[i] ]
+        }
+    }
+    return a
+}
+
+randomDeal = () => shuffle(new Array(52)
+    .fill('ion-ios-arrow-round-up', 0, 13)
+    .fill('ion-ios-arrow-round-down', 13, 26)
+    .fill('ion-ios-arrow-round-forward', 26, 39)
+    .fill('ion-ios-arrow-round-back', 39))
+
+class Arrow {
+    constructor() { this.el = el('i') }
+    update(data) { return setAttr(this.el, { className: data }) }
+}
+
+grid = list('.grid', Arrow)
+
+document.body.onclick = e => {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen()
+    }
+    grid.update(randomDeal())
+}
+
+mount(document.body, grid)
+document.body.onclick()
